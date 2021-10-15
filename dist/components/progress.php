927e45956833ba8570e1fbb3a5f94e96630cc088
Emran Khan <63547144+EmranWP@users.noreply.github.com>
2021-10-15 22:08:32 +0600

    Add files via upload

---
+++ dist/components/progress.php
@@ -0,0 +1,38 @@
+<div class="progress">
+    <div class="container">
+        <div class="top">
+            <h2>DEVELOPMENT PROGRESS</h2>
+            <div class="steps row">
+                <div class="step">1</div>
+                <div class="step">2</div>
+                <div class="step">3</div>
+                <div class="step">4</div>
+                <div class="step">5</div>
+            </div>
+        </div>
+        <div class="process_slider_container">
+            <div class="process_slider">
+                <div class="row">
+                    <div class="left col-60 col-sm">
+                        <h3>Step 4. Step description</h3>
+                        <p>Some description / Dates
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi egestas nulla et sollicitudin posuere. Maecenas libero odio, iaculis sit amet ipsum non, rutrum porttitor sem. </p>
+                    </div>
+                    <div class="right col-40  col-sm">
+                        <h2>01.11.2023</h2>
+                        <h2>01.04.2024</h2>
+                    </div>
+                </div>
+                <div class="navigation">
+                    <div class="prev nav_icon">
+                        <img src="../dist/img/icons/vector.svg" alt="">
+                    </div>
+                    <div class="next nav_icon">
+                        <img src="../dist/img/icons/vector.svg" alt="">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
